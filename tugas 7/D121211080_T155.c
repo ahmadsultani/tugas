4689e39df3a4e49e62c5dcd28cfedaf3f66d84d9
dayanullahas21d@student.unhas.ac.id <dayanullahas21d@student.unhas.ac.id>
2022-03-31 16:50:09 +0800

    update

--- tugas 7/D121211080_T155.c
+++ tugas 7/D121211080_T155.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int left = 0, right = sizeof(arr)/sizeof(arr[0]) - 1;
-    int target = 1;
+int interpolation_search(int arr[], int N, int target){
+    int left = 0, right = N - 1;
     while (left != right || arr[left] != arr[right]) {
         int mid = left + (right-left)/(arr[right]-arr[left]) * (target-arr[left]);
-        if (arr[mid] == target) {
-            printf("Target terletak pada indeks ke-%d\n", mid);
-            break;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
+    return -1; //target tidak ditemukan
+}
+
+int main() {
+    int N, target, index;
+    printf("Masukkan jumlah elemen: ");
+    scanf("%d", &N);
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        printf("Masukkan elemen ke-%d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Masukkan target pencarian: ");
+    scanf("%d", &target);
+    index = interpolation_search(arr, N, target);
+    if (index == -1) printf("Target tidak ditemukan\n");
+    else printf("Target ditemukan pada index %d\n", index);
     return 0;
 }
