0f9aa8f7881a2a42c80d5f6b5b9f847a2828ba94
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-05-08 00:07:09 +0800

    update

--- kelas b/tugas 3_2/QueueEmptyException.java
+++
@@ -1,5 +0,0 @@
-public class QueueEmptyException extends RuntimeException {
-    public QueueEmptyException (String err){
-        super(err);
-    }       
-}
--- kelas b/tugas 3_2/QueueFullException.java
+++
@@ -1,5 +0,0 @@
-public class QueueFullException extends RuntimeException {
-    public QueueFullException(String err){
-        super(err);
-    }
-}
