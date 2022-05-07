8a955e6c86836815dade810659bc3bd606327459
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-05-07 23:30:18 +0800

    update

---
+++ kelas b/tugas 3_2/Queue.java
@@ -0,0 +1,9 @@
+public interface Queue {
+    public int size();
+    public boolean isFull();
+    public boolean isEmpty();
+    public Object peek() throws QueueEmptyException;
+    public void enqueue(Object element) throws QueueFullException;
+    public Object dequeue() throws QueueEmptyException;
+}
+
---
+++ kelas b/tugas 3_2/QueueTest.java
@@ -0,0 +1,20 @@
+public class QueueTest {
+    public static void main(String[] args) {
+        int N = 9;
+        ArrayQueue aq = new ArrayQueue(N);
+        System.out.println("ENQUEUE Operation");
+        for (int i = 0; i<N; i++) {
+            aq.enqueue("Orang-"+(i+1));
+            System.out.print("Masuk ke antrian: "+"Orang-"+(i+1));
+            System.out.print(" Yang paling depan: "+aq.peek());
+            System.out.println(" Jumlah antrian: "+aq.size());
+        }
+
+        System.out.println("\nDEQUEUE Operation");
+        for (int i = 0; i<N; i++) {
+            System.out.println(" Yang paling depan: "+aq.peek());
+            System.out.print("Yang dilayani: "+aq.dequeue());
+            System.out.print(" Jumlah antrian: "+aq.size());
+        }
+    }
+}
