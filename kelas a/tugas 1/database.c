5d77bb8cf88534f4194d2320119b4bff50efe9fa
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-05-08 21:39:27 +0800

    update

---
+++ kelas a/tugas 1/database.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char nama[30];
+    char alamat[30];
+    char telpon[15];
+    char email[30];
+} Contacts;
+
+Contacts contacts[30];
+
+int main(){
+    int pilih, last_index = -1;
+    char lanjut;
+    do {
+        system("cls");
+        printf("Database Kontak Kelas A\n");
+        printf("=========================\n");
+        printf("1. Tambah Kontak\n");
+        printf("2. Tampilkan Kontak\n");
+        printf("3. Keluar\n");
+        printf("Pilih : ");
+        scanf("%d", &pilih);
+        switch (pilih) {
+            case 1:
+                last_index++;
+                printf("\nMenambah Kontak\n");
+                printf("====================\n");
+                printf("Nama : ");
+                fflush(stdin);
+                scanf("%[^\n]", contacts[last_index].nama);
+                printf("Alamat : ");
+                fflush(stdin);
+                scanf("%[^\n]", contacts[last_index].alamat);
+                printf("No. Telpon : ");
+                fflush(stdin);
+                scanf("%[^\n]", contacts[last_index].telpon);
+                printf("Email : ");
+                fflush(stdin);
+                scanf("%[^\n]", contacts[last_index].email);
+                break;
+            case 2:
+                printf("\nTampilkan Kontak\n");
+                printf("====================\n");
+                for (int i = 0; i <= last_index; i++) {
+                    printf("Nama : %s\n", contacts[i].nama);
+                    printf("Alamat : %s\n", contacts[i].alamat);
+                    printf("No. Telpon : %s\n", contacts[i].telpon);
+                    printf("Email : %s\n", contacts[i].email);
+                    printf("\n");
+                }
+                printf("Lanjut? ");
+                fflush(stdin);
+                scanf("%c", &lanjut); 
+                break;
+            case 3:
+                break;
+            default: 
+                printf("Invalid\n");
+        }
+    } while(pilih != 3);
+    printf("Selesai\n");
+    return 0;
+}
---
+++ kelas a/tugas 1/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+==\n");
+                for (int i = 0; i <= last_index; i++) {
+                    printf("Nama : %s\n", contacts[i].nama);
+                    printf("Alamat : %s\n", contacts[i].alamat);
+                    printf("No. Telpon : %s\n", contacts[i].telpon);
+                    printf("Email : %s\n", contacts[i].email);
+                    printf("\n");
+                }
+                printf("Lanjut? ");
